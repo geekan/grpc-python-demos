52ee214cefdb0135483b23c764656390dc81a2de
geekan <geekan@foxmail.com>
2015-09-04 15:21:07 +0000

    change filename

--- helloworld/client.py
+++ helloworld/client.py
@@ -6,7 +6,7 @@ _TIMEOUT_SECONDS = 10
 
 
 def run():
-    with helloworld_pb2.early_adopter_create_HelloService_stub('localhost', 50051) as stub:
+    with helloworld_pb2.early_adopter_create_helloworld_stub('localhost', 50051) as stub:
         response = stub.SayHello(helloworld_pb2.HelloRequest(name='you', message='hey dude'), _TIMEOUT_SECONDS)
         print(response)
 
--- helloworld/helloworld_server.py
+++ helloworld/helloworld_server.py
@@ -7,16 +7,16 @@ import helloworld_pb2
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
 
-class HelloService(helloworld_pb2.EarlyAdopterhelloworldServicer):
+class helloworld(helloworld_pb2.EarlyAdopterhelloworldServicer):
 
     def SayHello(self, request, context):
-        print("request: " + str(request))
+        print("request: " + str(request)) 
         return helloworld_pb2.HelloReply(message='%s, %s!' % (request.message, request.name))
 
 
 def serve():
     server = helloworld_pb2.early_adopter_create_helloworld_server(
-            HelloService(), 50051, None, None)
+            helloworld(), 50051, None, None)
     server.start()
     try:
         while True:
